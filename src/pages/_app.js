f15d78f3eede1f2589b3fe0bd9c22416a4151b71
Bob Fornal <bob.fornal@leadingedje.com>
2023-03-25 09:26:50 -0400

    setting base href

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import '@core/styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
+
+const isProduction = globalThis.location?.hostname.includes('github.io');
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        { isProduction ? <base href="https://bob-fornal.github.io/bob-portfolio/" /> : null }
+      </Head>
       <Component {...pageProps} />;
       <Analytics />
     </>
