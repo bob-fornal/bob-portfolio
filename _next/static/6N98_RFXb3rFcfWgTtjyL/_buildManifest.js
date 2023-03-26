2e16cac400399bbad84ee4af9544640770ee7f1b
bob-fornal <bob-fornal@users.noreply.github.com>
2023-03-26 19:51:28 +0000

    deploy: 53c8f10d783746d5b7a5a9000f54a5d4b6ddc6ba

--- _next/static/6N98_RFXb3rFcfWgTtjyL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/951-3a42842364ddb692.js","static/css/0645977bdc14d8e8.css","static/chunks/pages/index-77b833461833cefe.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/PGMjjs2vSTJzvnjYrpfbg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/951-3a42842364ddb692.js","static/css/0645977bdc14d8e8.css","static/chunks/pages/index-77175354f9e8b961.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
