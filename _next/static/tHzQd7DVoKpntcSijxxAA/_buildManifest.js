611fe935ec99b395e989ba0efa3339343666f5a7
bob-fornal <bob-fornal@users.noreply.github.com>
2023-03-28 01:35:07 +0000

    deploy: 6a650fbec21fc9989359add4d4caf14c11dd5d33

---
+++ _next/static/tHzQd7DVoKpntcSijxxAA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/818-f381d5ff5f3c584e.js","static/css/0645977bdc14d8e8.css","static/chunks/pages/index-068c3bdca8ce1097.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
